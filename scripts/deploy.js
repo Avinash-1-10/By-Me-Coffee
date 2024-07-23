4753b5c003049d9b72215bb45b9717115ce33a5a
Avinash-1-10 <avinashyede110@gmail.com>
2024-07-23 22:59:05 +0530

    fix bugs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ const hre = require('hardhat');
 
 async function getBalance(address) {
   const balance = await hre.ethers.provider.getBalance(address);
-  return hre.ethers.utils.formatEther(balance);
+  return hre.ethers.formatEther(balance);
 }
 
 async function consoleBalances(addresses) {
@@ -27,23 +27,29 @@ async function main() {
   const [owner, from1, from2, from3] = await hre.ethers.getSigners();
   const contractFactory = await hre.ethers.getContractFactory('Coffee');
   const contract = await contractFactory.deploy();
-  await contract.deployed();
- 
-  console.log(`Address of contract is: ${contract.address}`);
-  console.log(`Contract balance: `, await getBalance(contract.address));
+  // await contract.deployed();
+
+  // console.log(await contract.getAddress());
+  console.log(`Address of contract is: ${await contract.getAddress()}`);
+  console.log(
+    `Contract balance: `,
+    await getBalance(await contract.getAddress())
+  );
   console.log(`Owner balance: `, await getBalance(owner.address));
 
- const addresses = [owner.addresss, from1.address]
- await consoleBalances(addresses); 
+  const addresses = [await owner.getAddress(), from1.address];
+  console.log(addresses);
+  console.log('Before bying coffee');
+  await consoleBalances(addresses);
 
- const amount = {value:hre.ethers.utils.parseEther('0.01')};
- await contract.connect(from1).byCoffee("From 1", "Very Nice Coffee",amount);
- await contract.connect(from2).byCoffee("From 2", "Very Nice Hardhat",amount);
- await contract.connect(from3).byCoffee("From 3", "Very Nice Solidity",amount);
- const memos = await contract.getMemos();
- consoleMemos(memos); 
- await consoleBalances(addresses);
- 
+  const amount = { value: hre.ethers.parseEther('0.01') };
+  await contract.connect(from1).byCoffee('From 1', 'Very Nice Coffee', amount);
+  await contract.connect(from2).byCoffee('From 2', 'Very Nice Hardhat', amount);
+  await contract
+    .connect(from3)
+    .byCoffee('From 3', 'Very Nice Solidity', amount);
+  console.log('After bying coffee');
+  await consoleBalances(addresses);
 }
 
 main().catch((error) => {
