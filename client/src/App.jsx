d39f5d1275e83f2ef782db38c18104f2857ac4f5
Avinash-1-10 <avinashyede110@gmail.com>
2024-07-29 23:38:04 +0530

    add connect wallet functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import contractAbi,{abi} from '../../artifacts/contracts/Coffee.sol/Coffee.json';
+import contractAbi from '../../artifacts/contracts/Coffee.sol/Coffee.json';
 
 const App = () => {
-  console.log(abi)
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+
+  const connectWallet = async () => {
+    const contractAddress = '0x4414e0Cb6A94FD93CEB5A84608023bC19e867F4A';
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        const provider = new ethers.BrowserProvider(ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, contractAbi.abi, signer);
+        setState({
+          provider,
+          signer,
+          contract,
+        });
+        console.log({
+          provider,
+          signer,
+          contract,
+        });
+      } else {
+        console.log('Ethereum object not found, install Metamask.');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
   return <div>App</div>;
 };
 
